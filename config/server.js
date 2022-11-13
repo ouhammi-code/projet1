cbc6b66d9ca123ee5bb16d6100d8dcefb28573ac
PC <ouhammimed98@gmail.com>
2022-11-13 21:47:40 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7a12339521161b808caee57c944f35b7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ae158e6f-eb7a-4170-abbc-bcb4f3678251'
+};
